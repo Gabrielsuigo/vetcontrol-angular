0f5b9df29fef750d1f793c212478d3a902057dac
Gabriel Suigo <gaby.jjr10@gmail.com>
2026-06-05 16:30:02 -0300

    agregue footer

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -2,14 +2,14 @@ import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-
+import { Footer } from '../../components/footer/footer';
 
 @Component({
   selector: 'app-home',
 
   standalone: true,
 
-  imports: [RouterModule, MatButtonModule, MatIconModule],
+  imports: [RouterModule, MatButtonModule, MatIconModule, Footer],
 
   templateUrl: './home.html',
 
