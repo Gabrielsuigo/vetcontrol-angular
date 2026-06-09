1ef74e86fce2c0c6b7e9d223c36956b28d226e6e
Gabriel Suigo <gaby.jjr10@gmail.com>
2026-06-08 21:39:17 -0300

    feat: mejora dashboard mascota y consultas veterinarias

--- src/app/pages/detalle-mascota/detalle-mascota.ts
+++ src/app/pages/detalle-mascota/detalle-mascota.ts
@@ -2,6 +2,7 @@ import { Component, inject, computed } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { MascotaService } from '../../core/services/mascota.service';
 import { MatIconModule } from '@angular/material/icon';
+import { TurnoService } from '../../core/services/turno.service';
 
 @Component({
   selector: 'app-detalle-mascota',
@@ -19,7 +20,26 @@ export class DetalleMascota {
 
   mascotaService = inject(MascotaService);
 
+  turnoService = inject(TurnoService);
+
   id = Number(this.route.snapshot.paramMap.get('id'));
 
   mascota = computed(() => this.mascotaService.mascotas().find((m) => m.id === this.id));
+
+  turnosMascota = computed(() =>
+    this.turnoService.turnosUsuario().filter((t) => t.mascotaId === this.id),
+  );
+  totalVacunas = computed(() => this.mascota()?.vacunas.length ?? 0);
+
+  totalConsultas = computed(() => this.mascota()?.consultas.length ?? 0);
+
+  totalTurnos = computed(() => this.turnosMascota().length);
+
+  ultimoPeso = computed(() => {
+    const consultas = this.mascota()?.consultas ?? [];
+
+    if (consultas.length === 0) return null;
+
+    return consultas[consultas.length - 1].peso;
+  });
 }
