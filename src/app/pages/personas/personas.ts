bc6cb1cbe57dae7d2e9c0e84d32fb6f12ee5863a
Gabriel Suigo <gaby.jjr10@gmail.com>
2026-07-11 11:46:08 -0300

    mejore diseño de formulario de registro de mascota

--- src/app/pages/personas/personas.ts
+++ src/app/pages/personas/personas.ts
@@ -1,15 +1,46 @@
 import { Component } from '@angular/core';
-import { PersonaForm } from '../../components/persona-form/persona-form';
 import { PersonaList } from '../../lista-personas/lista-personas';
 import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDialog } from '@angular/material/dialog';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+
 import { MascotaService } from '../../core/services/mascota.service';
+import { PersonaForm } from '../../components/persona-form/persona-form';
 
 @Component({
   selector: 'app-mascotas',
   standalone: true,
-  imports: [PersonaForm, PersonaList, MatCardModule],
+  imports: [
+    PersonaList,
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule,
+    FormsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatSelectModule,
+  ],
   templateUrl: './personas.html',
+  styleUrls: ['./personas.css'],
 })
 export class Mascotas {
-  constructor(public personaService: MascotaService) {}
+  constructor(
+    public personaService: MascotaService,
+    private dialog: MatDialog,
+  ) {}
+
+  abrirFormulario() {
+    this.dialog.open(PersonaForm, {
+      width: '900px',
+
+      maxWidth: '95vw',
+
+      autoFocus: false,
+    });
+  }
 }
