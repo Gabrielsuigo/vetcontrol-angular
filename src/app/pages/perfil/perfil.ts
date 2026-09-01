588beea5ff13e6f1e1ee9e29db52b2bd0ef73db6
Gabriel Suigo <gaby.jjr10@gmail.com>
2026-09-01 11:09:22 -0300

    modifique perfil

--- src/app/pages/perfil/perfil.ts
+++ src/app/pages/perfil/perfil.ts
@@ -1,18 +1,22 @@
 import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
 
 import { AuthService } from '../../core/auth/services/auth.service';
+import { EmpresaService } from '../../core/services/empresa.service';
 
 @Component({
   selector: 'app-perfil',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule, MatIconModule],
   templateUrl: './perfil.html',
   styleUrl: './perfil.css',
 })
 export class Perfil {
   authService = inject(AuthService);
-  usuario = this.authService.usuarioActual;
+  empresaService = inject(EmpresaService);
 
-  fotoPerfil = 'https://ui-avatars.com/api/?background=6366F1&color=fff&size=256&name=Gabriel';
+  usuario = this.authService.usuarioActual;
+  empresa = this.empresaService.empresa;
 }
